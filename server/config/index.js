f8b87f86398ceeb95e2565550c8892fad68c270a
Vahan <melikyanvl@gmail.com>
2018-07-16 20:02:35 +0400

    test

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     'port': process.env.PORT || 3000,
-    'db': process.env.MONGODB_URI || 'mongodb://localhost:27017/newDB'
+    'db': process.env.MONGODB_URI || 'https://codeep-hr-tool.herokuapp.com'
 };
 
